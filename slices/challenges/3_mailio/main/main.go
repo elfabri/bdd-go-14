9ed343a69b0cb14bea3d9cbb39baa6eee4a2eed2
elfabri <navarro.fabricio.nahuel@gmail.com>
2025-01-12 09:57:18 -0300

    slices challenges 3

---
+++ slices/challenges/3_mailio/main/main.go
@@ -0,0 +1,18 @@
+/*
+                *** Mailio ***
+Textio is launching a new email messaging product, "Mailio"!
+
+Fix the compile-time bug in the getFormattedMessages function.
+The function body is correct, but the function signature is not.
+
+*/
+
+package main
+
+func getFormattedMessages(messages []string, formatter func(string) string) []string {
+	formattedMessages := []string{}
+	for _, message := range messages {
+		formattedMessages = append(formattedMessages, formatter(message))
+	}
+	return formattedMessages
+}
